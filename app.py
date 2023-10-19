0179b8a63e6d345045e2a5869a5922056e1f5c07
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-10-19 07:01:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def on_message(client, userdata, message):
 
 broker="157.230.214.127"
 port=1883
-client1= paho.Client("ESTE_ES_MAURI")
+client1= paho.Client("ESTE_ES_MAURI2")
 client1.on_message = on_message
 
 
@@ -29,7 +29,7 @@ st.title("MQTT Control")
 
 if st.button('ON'):
     act1="ON"
-    client1= paho.Client("ESTE_ES_MAURI")                           
+    client1= paho.Client("ESTE_ES_MAURI2")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
@@ -43,7 +43,7 @@ else:
 
 if st.button('OFF'):
     act1="OFF"
-    client1= paho.Client("ESTE_ES_MAURI")                           
+    client1= paho.Client("ESTE_ES_MAURI2")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
@@ -57,7 +57,7 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("ESTE_ES_MAURI")                           
+    client1= paho.Client("ESTE_ES_MAURI2")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
